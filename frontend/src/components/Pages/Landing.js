5d130f8d01a8642a40541ac393a956e47791f570
Katrina Orevillo (She/Her) <58285120+korevillo@users.noreply.github.com>
2021-10-29 15:24:08 -0700

    emotion/react and emotion/styled for mui

--- frontend/src/components/Pages/Landing.js
+++ frontend/src/components/Pages/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./style.css"
+import Button from '@mui/material/Button';
 
 export default function Landing() {
     return (
         <div class = "center">
             <h1>Cauldron</h1>
             <h2>No tricks, only treats.</h2>
+            <Button variant="outlined">Start Tracking</Button>
         </div>
     )
 }
