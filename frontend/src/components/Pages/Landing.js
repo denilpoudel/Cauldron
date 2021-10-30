57eb2a0fba4f17f5f662818ebb6d09320857b5c4
AdamSerag-cloud <adamserag25@gmail.com>
2021-10-29 18:53:13 -0700

    added link to the landing page button to take you to the next page

--- frontend/src/components/Pages/Landing.js
+++ frontend/src/components/Pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./style.css"
 //Button alterations
+import {Link} from 'react-router-dom';
 import Button from '@mui/material/Button';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
@@ -10,9 +11,11 @@ export default function Landing() {
         <div class = "center">
             <h1>Cauldron</h1>
             <h2>No tricks, only treats.</h2>
-            <Button variant="outlined" endIcon={<ArrowForwardIcon />}>
-                Start Tracking
-            </Button>
+            <Link to="/candylimits">
+                <Button variant="contained" endIcon={<ArrowForwardIcon />}>
+                    Start Tracking
+                </Button>
+            </Link>
         </div>
     )
 }
