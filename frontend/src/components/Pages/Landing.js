0b60766d666068a24488fee6f9b8545373bd8f77
AdamSerag-cloud <adamserag25@gmail.com>
2021-10-29 23:29:16 -0700

    edited landing page start button to look more like the figma

--- frontend/src/components/Pages/Landing.js
+++ frontend/src/components/Pages/Landing.js
@@ -12,7 +12,7 @@ export default function Landing() {
             <h1>Cauldron</h1>
             <h2>No tricks, only treats.</h2>
             <Link to="/candylimits">
-                <Button variant="contained" endIcon={<ArrowForwardIcon />}>
+                <Button style={{backgroundColor: "#cbc3e3", color: "#9370D0", fontSize: "20px",  fontWeight: "bold"}}variant="contained" endIcon={<ArrowForwardIcon />}>
                     Start Tracking
                 </Button>
             </Link>
