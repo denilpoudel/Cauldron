8a764afa701eb6ccf25e5fdef303f21cdd8117f8
AdamSerag-cloud <adamserag25@gmail.com>
2021-10-29 17:57:16 -0700

    updated index.js with other pages and created the loading and tracker pages. Also set up react router in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import {Landing, CandyLimits} from "./components/Pages"
+import {Landing, CandyLimits, Loading, Tracker} from "./components/Pages"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <CandyLimits></CandyLimits>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+              <Landing />
+          </Route>
+          <Route path ="/candylimits" component = {CandyLimits} />
+          <Route path ="/loading" component = {Loading} />
+          <Route path ="/tracker" component = {Tracker} />
+        </Switch>
+      </Router>
     </div>
   );
 }
