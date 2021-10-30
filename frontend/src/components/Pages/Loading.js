304a76c7f808e1913b2d2547453e1e389feb6cc4
AdamSerag-cloud <adamserag25@gmail.com>
2021-10-30 10:27:38 -0700

    Adjusted the landing page color palette and added background candies

--- frontend/src/components/Pages/Loading.js
+++ frontend/src/components/Pages/Loading.js
@@ -5,7 +5,6 @@ export default function Loading() {
     return (
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
             <h4>Preparing your Cauldron...</h4>
-
         </div>
     )
 }
